5e02b27d19268fe9fafbf7ec64b9f16c555bc0f7
Ethan Hu <fendouhyz@163.com>
2020-10-21 12:37:37 +0800

    test: fmt ropsten test

--- scripts/testnet-ropsten/test-locker-all.js
+++ scripts/testnet-ropsten/test-locker-all.js
@@ -4,29 +4,43 @@
 // Runtime Environment's members available in the global scope.
 const bre = require("@nomiclabs/buidler");
 
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 async function main() {
     // Buidler always runs the compile task when running scripts through it.
     // If this runs in a standalone fashion you may want to call compile manually
     // to make sure everything is compiled
     // await bre.run('compile');
 
-    // mock spv
-    const mockSpvAddr = '0x86F401fD9c55bD919f0b5D9e908B9DEE4e006D31';
+    // deploy MockCKBSpv
+    const MockCKBSpv = await ethers.getContractFactory("MockCKBSpv");
+    const mockSpv = await MockCKBSpv.deploy();
+    await mockSpv.deployed();
+    const mockSpvAddr = mockSpv.address;
+    console.log("mockSpv deployed to:", mockSpvAddr);
 
-    // We get the contract to deploy
+    // deploy TokenLocker
     const TokenLocker = await ethers.getContractFactory("TokenLocker");
     const locker = await TokenLocker.deploy(mockSpvAddr, 20);
     await locker.deployed();
-    console.log("locker deployed to:", locker.address);
+    const lockerAddr = locker.address;
+    console.log("locker deployed to:", lockerAddr);
+    console.log("waiting for block confirmations, about 1 minute")
+    await sleep(60 * 1000);
 
-    // lockETH 1.34
-    let amount = ethers.utils.parseEther("0.34");
-    let res = await locker.lockETH(amount, "000111 for lockETH test", {value: amount})
+    // lockETH 0.123
+    let amount = ethers.utils.parseEther("0.123");
+    let res = await locker.lockETH(amount, "0.123 ether just for lockETH test", {value: amount})
     console.log("lockETH res: ", res)
+    console.log("waiting for block confirmations, about 1 minute")
+    await sleep(60 * 1000);
 
     // unlockETH
-    res = await locker.unlockToken([0,0,0,1,1,1])
+    res = await locker.unlockToken([0])
     console.log("unlockETH res: ", res)
+
 }
 
 // We recommend this pattern to be able to use async/await everywhere
